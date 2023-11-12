68120e958ec2c82d13991c6a02b1e13336852670
olgazh565 <olgazh565@gmail.com>
2023-11-12 22:02:25 +0300

    task complete

---
+++ src/modules/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import methed from '../img/methed.jpg';
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img width={500} src={methed}/>
+  </>
+);
+
+export default Module;
